8038ca00c485a300243f758c9e081a3e0493675c
Jannie Theunissen <jannie@onesheep.org>
2021-01-23 13:40:13 +0200

    MemberDate model

--- lib/models.dart
+++ lib/models.dart
@@ -2,17 +2,31 @@ class Member {
   String name;
   String imageUrl;
   String description;
-  DateTime due;
+  List<MemberDate> dates;
 
   Member.fromJson(Map<String, dynamic> json) {
     name = json["name"];
     description = json["desc"];
     final utc = json["due"];
-    due = (utc == null) ? null : DateTime.parse(utc);
     final attachments = json["attachments"].map((entry) {
       return entry["url"].toString();
     }).toList();
     imageUrl = attachments.isEmpty ? null : attachments[0];
+
+    // parse any dates
+    dates = [];
+    final lists = json["checklists"].map((list) {
+      return list["checkItems"];
+    }).toList();
+    if (lists?.isEmpty ?? true) return;
+    if (lists[0]?.isEmpty ?? true) return;
+    dates = lists[0]
+        .map((row) {
+          final item = MemberDate.fromListEntry(row["name"]);
+          return item;
+        })
+        .cast<MemberDate>()
+        .toList();
   }
 
   static List<Member> formList(List<dynamic> json) {
@@ -28,5 +42,23 @@ class Member {
     final counter = theDay.difference(DateTime.now()).inDays;
     final index = counter % members.length;
     return members[index];
+  }
+}
+
+class MemberDate {
+  int year;
+  int month;
+  int day;
+  String description;
+
+  MemberDate.fromListEntry(String entry) {
+    final parts = entry.split(":");
+    if (parts.length != 2) return;
+    final dateParts = parts[0].split("-");
+    if (dateParts.length != 3) return;
+    description = parts[1].trim();
+    year = int.tryParse(dateParts[0]);
+    month = int.tryParse(dateParts[1]);
+    day = int.tryParse(dateParts[2]);
   }
 }
